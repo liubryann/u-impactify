3749beac54502dabe1e8968e6f55b4c979fc6e21
ivinable2000 <44382576+ivinable2000@users.noreply.github.com>
2020-10-09 23:22:37 -0400

    DON-29 Added ThemeProvider component and created theme variable

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,32 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 import './App.css';
 // Pages
 import Login from './pages/Login';
 
+const theme = createMuiTheme({
+  palette: {
+    type: 'light',
+    primary: {
+      main: '#1976d2',
+    },
+    background: {
+      default: '#fafafa',
+      paper: '#fff',
+    },
+  },
+});
+
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/login" render={() => <Login/>}/>
-      </Switch>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Switch>
+          <Route exact path="/login" render={() => <Login/>}/>
+        </Switch>
+      </Router>
+    </ThemeProvider>
   );
 }
 
