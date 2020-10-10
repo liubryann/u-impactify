48a9ced3e4710e76fb540c485b9e5b00d56f962d
Bryan Liu <liubryan.developer@gmail.com>
2020-10-10 14:56:22 -0400

    DON-25 added signup data validator and usertype string constants

--- backend/functions/handlers/users.js
+++ backend/functions/handlers/users.js
@@ -1,4 +1,5 @@
 const { admin, db } = require("../util/admin");
+const { validateSignupData } = require('../util/validators');
 
 const config = require("../util/config");
 
@@ -8,15 +9,17 @@ firebase.initializeApp(config);
 // Sign up 
 exports.signup = (req, res) => {
     const newUser = {
-        name: req.body.name,
-        body: req.body.body
+        type: req.body.type, 
+        org: req.body.org,
+        first: req.body.first,
+        last: req.body.last, 
+        email: req.body.email, 
+        password: req.body.password, 
+        confirmPassword: req.body.confirmPassword
     }
 
-    db.doc(`/test/${newUser.name}`).set(newUser)
-    .then(() => {
-        return res.status(200).json({ good: "shit"});
-    })
-    .catch(err => {
-        res.status(400).json({err});
-    })
+    const { valid, errors } = validateSignupData(newUser); 
+    if(!valid) return res.status(400).json(errors); 
+
+  
 }
---
+++ backend/functions/util/validators.js
@@ -0,0 +1,43 @@
+const {userTypes} = require('./constants'); 
+
+const isEmail = (email) => {
+    const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return email.match(regEx);
+}
+
+const isEmpty = (string) => {
+    return string.trim() === ''; 
+}
+
+exports.validateSignupData = (data) => {
+    let errors = {}; 
+
+    if (data.type === userTypes.SOCIAL_INITIATIVE) {
+        if(isEmpty(data.org)) {
+            errors.org = 'Must not be empty';
+        }
+    }
+    if (isEmpty(data.email)) {
+        errors.email = 'Must not be empty'; 
+    }
+    else if (!isEmail(data.email)) {
+        errors.email = 'Must be a valid email address';
+    }
+    if (isEmpty(data.first)) {
+        errors.first = 'Must not be empty'; 
+    }
+    if (isEmpty(data.last)) {
+        errors.last = 'Must not be empty'; 
+    }
+    if (isEmpty(data.password)) {
+        errors.password = 'Must not be empty'; 
+    }
+    if (data.password !== data.confirmPassword) {
+        errors.confirmPassword = 'Passwords must match'; 
+    }
+    
+    return {
+        errors, 
+        valid: Object.keys(errors).length === 0 ? true : false
+    }
+}
