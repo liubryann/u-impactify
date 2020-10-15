31b47227a1bb88faca1bc48c37755722efc31856
ivinable2000 <44382576+ivinable2000@users.noreply.github.com>
2020-10-15 13:19:16 -0400

    DON-39 fixed thunk bugs for import and composing (#8)

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,10 +1,10 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
-import thunk from 'redux';
+import thunk from 'redux-thunk';
 import userReducer from './reducers/userReducer';
 
 const initialState = {};
 
-const middleware = {thunk};
+const middleware = [thunk];
 
 const reducers = combineReducers({
     user: userReducer,
@@ -15,7 +15,7 @@ const store = createStore(
     initialState, 
     compose(
         applyMiddleware(...middleware)
-        )
+    )
 );
 
 export default store;
