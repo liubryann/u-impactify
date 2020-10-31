09fc4fbc13ebc1b96d0c92d95f87500e696afc74
HarryZ7G <56236602+HarryZ7G@users.noreply.github.com>
2020-10-30 21:48:02 -0400

    Don-52 Started implementation of Redux

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react'
 import Avatar from '@material-ui/core/Avatar'
 import Box from '@material-ui/core/Box'
-import CssBaseline from "@material-ui/core/CssBaseline"
-import Card from "@material-ui/core/Card"
-import Grid from '@material-ui/core/Grid'
-import { makeStyles } from '@material-ui/core/styles'
+import withStyles from "@material-ui/core/styles/withStyles";
 import NavBar from '../../components/Navbar'
 import { Typography } from '@material-ui/core'
 
-const useStyles = makeStyles((theme) => ({
+import { connect } from 'react-redux';
+import { getAuthenticatedUserData } from '../../redux/actions/userActions';
+import store from '../../redux/store';
+// Link to the dashboard!
+const styles = (theme) => ({
     root: {
         height: "100%",
         width: "100%",
@@ -51,42 +52,59 @@ const useStyles = makeStyles((theme) => ({
         height: theme.spacing(25),
         padding: '30px',
     },
-}))
+});
 
-function Profile() {
-    const classes = useStyles();    
-      return (
-        <div>
-            <NavBar/>
-            <Box className={classes.root}>
-                <Box className={classes.profile}>
-                    <Box pt={10} pb={4} p={15}>
-                        <Avatar alt="Bob" className={classes.userIcon}/>
+class Profile extends Component {
+    componentDidMount(){
+        console.log("component did mount");
+        this.props.getAuthenticatedUserData();
+    }
+
+    render () {
+        const { classes } = this.props;
+        const { userDetails } = this.props.user
+        console.log(store.getState());
+        return(
+            <div>
+                <NavBar/>
+                <Box className={classes.root}>
+                    <Box className={classes.profile}>
+                        <Box pt={10} pb={4} p={15}>
+                            <Avatar alt="Bob" className={classes.userIcon}/>
+                        </Box>
+                        <Box p={1}>
+                            <Typography variant="h5"> {userDetails.name} </Typography>
+                        </Box>
+                        <Box p={1}>
+                            <Typography variant="h5"> {userDetails.email} </Typography>
+                        </Box>
                     </Box>
-                    <Box p={1}>
-                        <Typography variant="h5"> John Buck </Typography>
-                    </Box>
-                    <Box p={1}>
-                        <Typography variant="h5"> john@email.com </Typography>
+                    <Box mt={10} ml={5} mr={3} p={5} flexGrow={1} className={classes.roundBox}>
+                        <Box>
+                            <Typography variant="h4"> Who am I? </Typography>
+                        </Box>
+                        <Box mt={2} p={2}  className={classes.who}>
+                            <Typography variant="h4"> I don't even know who I am </Typography>
+                        </Box>
+                        <Box pt={7}>
+                            <Typography variant="h4"> Introduction </Typography>
+                        </Box>
+                        <Box mt={2} p={2} mb={3} className={classes.intro}>
+                            <Typography variant="h4"> I want coffee </Typography>
+                        </Box>
                     </Box>
                 </Box>
-                <Box mt={10} ml={5} mr={3} p={5} flexGrow={1} className={classes.roundBox}>
-                    <Box>
-                        <Typography variant="h4"> Who am I? </Typography>
-                    </Box>
-                    <Box mt={2} p={2}  className={classes.who}>
-                        <Typography variant="h4"> I don't even know who I am </Typography>
-                    </Box>
-                    <Box pt={7}>
-                        <Typography variant="h4"> Introduction </Typography>
-                    </Box>
-                    <Box mt={2} p={2} mb={3} className={classes.intro}>
-                        <Typography variant="h4"> I want coffee </Typography>
-                    </Box>
-                </Box>
-            </Box>
-        </div>
-    )
+            </div>
+        )
+    }
 }
 
-export default Profile
+const mapDispatchToProps = {
+    getAuthenticatedUserData: getAuthenticatedUserData
+};
+
+const mapStateToProps = (state) => ({
+    user: state.user
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(withStyles(styles)(Profile));
